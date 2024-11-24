0688fd37af1bc44e140c13b0aba341f3f99fd45c
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-11-24 19:08:01 +0530

    file-system:verify-redux-fixed

--- src/app/auth/verify/_components/index.tsx
+++ src/app/auth/verify/_components/index.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import React, { useEffect, Suspense } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/redux/store";
-import { verifyUser } from "@/redux/verify/thunk";
-import FSLogoFrame from "@/components/ui/FS-Logo";
-import icons, { IconType } from "@/utils/icons";
-import { Divider } from "@nextui-org/divider";
+import React, { useEffect, Suspense } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/redux/store';
+import { resetVerifyState } from '@/redux/verify/slice';
+import { ApiStatusEnum } from '@/interface/interface';
+import FSLogoFrame from '@/components/ui/FS-Logo';
+import icons, { IconType } from '@/utils/icons';
+import { Divider } from '@nextui-org/divider';
+import { verifyUser } from '@/redux/verify/thunk';
 
 const VerifyPage = () => {
   const router = useRouter();
@@ -19,44 +21,41 @@ const VerifyPage = () => {
   );
 
   useEffect(() => {
-    const token = params.get("token");
+    const token = params.get('token');
     if (token) {
       dispatch(verifyUser(token as string));
     }
   }, [params, dispatch]);
 
   useEffect(() => {
-    if (status === "succeeded") {
+    if (status === ApiStatusEnum.SUCCEEDED) {
       router.push('/auth/login');
+      dispatch(resetVerifyState());
     }
   }, [status, router]);
 
   return (
     <React.Fragment>
-      <div className={`w-full h-auto flex justify-center items-center`}>
+      <div className="w-full h-auto flex justify-center items-center">
         <FSLogoFrame />
       </div>
-      <Divider className={`mt-1`} />
+      <Divider className="mt-1" />
       <div className="w-full h-auto my-5 flex justify-center items-center flex-col gap-2">
-        {status === "loading" && (
+        {status === ApiStatusEnum.LOADING && (
           <div className="bg-background flex justify-center items-center gap-3">
             <span className="min-w-5 min-h-5 rounded-full animate-spin border-4 border-primary-200 border-t-primary"></span>
             <span>Verifying...</span>
           </div>
         )}
-        {status === "succeeded" && (
+        {status === ApiStatusEnum.SUCCEEDED && (
           <div className="bg-background flex justify-center items-center gap-3">
-            <div className="text-success">
-              {icons(IconType.SUCCESS)}
-            </div>
+            <div className="text-success">{icons(IconType.SUCCESS)}</div>
             <span>Verified</span>
           </div>
         )}
-        {status === "failed" && (
+        {status === ApiStatusEnum.FAILED && (
           <div className="bg-background flex justify-center items-center gap-3">
-            <div className="text-warning">
-              {icons(IconType.WARNING)}
-            </div>
+            <div className="text-warning">{icons(IconType.WARNING)}</div>
             <span>Invalid Token</span>
           </div>
         )}
